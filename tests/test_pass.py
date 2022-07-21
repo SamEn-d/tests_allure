2510145ca17c43199849e22bda001eb2ea498ff1
Sam <sane10@ya.ru>
2022-07-21 22:13:05 +0300

    Ner

--- tests/test_pass.py
+++ tests/test_pass.py
@@ -4,9 +4,9 @@ from allure_commons.types import Severity
 @allure.tag('GitHub')
 @allure.severity(Severity.MINOR)
 @allure.label('owner','Sam')
-@allure.feature('Задачи Issues в репозитории')
-@allure.story('Проверка Issues через PageObject')
-@allure.title('Проверка Issues через PageObject Title')
+@allure.feature('Issues')
+@allure.story('Issues PageObject')
+@allure.title('Issues PageObject Title')
 def test_git_allure_steps():
     pass
 #s
