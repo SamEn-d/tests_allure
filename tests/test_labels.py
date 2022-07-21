6bafc14696a2c7e9dc5b9703cf926cf6dc5e0bdc
Sam <sane10@ya.ru>
2022-07-21 17:59:30 +0300

    Ner

---
+++ tests/test_labels.py
@@ -0,0 +1,13 @@
+import allure
+from allure_commons.types import Severity
+
+@allure.tag('GitHub')
+@allure.severity(Severity.MINOR)
+@allure.label('owner','Sam')
+@allure.feature('Задачи Issues в репозитории')
+@allure.story('Проверка Issues через PageObject')
+@allure.title('Проверка Issues через PageObject Title')
+def test_git_allure_steps():
+    pass
+#s
+
